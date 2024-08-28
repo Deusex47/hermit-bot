afb388595fe499f70d2b5360bd286c6983b54902
mert ozturk <66350610+Deusex47@users.noreply.github.com>
2024-08-28 23:39:51 +0200

    Update config.js

--- config.js
+++ config.js
@@ -16,7 +16,7 @@ const DEBUG = convertToBool(process.env.DEBUG, 'true');
 
 module.exports = {
   VERSION: 'v4.4.4',
-  SESSION_ID: process.env.SESSION_ID || '',
+  SESSION_ID: process.env.SESSION_ID || 'HQ_2024_AUG_28_H23XSL',
   MODE: (process.env.MODE || 'private').toLowerCase(),
   HANDLERS: (process.env.PREFIX || '^[.,!]').trim(),
   SEND_READ: toBool(process.env.READ_COMMAND),
